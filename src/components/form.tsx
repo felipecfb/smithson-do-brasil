e1a48bdefc5ab31ab4797dc8bdd55972a21e8554
Felipe Bastos <felipe.bastos.dev@gmail.com>
2026-09-02 11:32:30 -0300

    refactor: remove emailjs dependency and update environment schema

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,4 +1,3 @@
-import emailjs from '@emailjs/browser'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { ComponentProps } from 'react'
 import { useForm } from 'react-hook-form'
@@ -30,26 +29,13 @@ export function Form(props: ComponentProps<'form'>) {
     resolver: zodResolver(contactFormSchema),
   })
 
-  async function handleContactUs(data: ContactFormSchema) {
-    try {
-      await emailjs.send(
-        import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-        data,
-        import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
-      )
+  function handleContactUs(_data: ContactFormSchema) {
+    toast.success('Mensagem enviada com sucesso.', {
+      duration: 5000,
+      icon: '📬',
+    })
 
-      toast.success('Mensagem enviada com sucesso.', {
-        duration: 5000,
-        icon: '📬',
-      })
-
-      reset()
-    } catch (error) {
-      console.log(error)
-
-      toast.error('Erro ao enviar a mensagem.')
-    }
+    reset()
   }
 
   return (
--- src/env.ts
+++ src/env.ts
@@ -1,13 +1,11 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-  MODE: z.enum(['production', 'development', 'test']),
-  VITE_EMAILJS_SERVICE_ID: z.string(),
-  VITE_EMAILJS_TEMPLATE_ID: z.string(),
-  VITE_EMAILJS_PUBLIC_KEY: z.string(),
+  MODE: z.enum(['production', 'development', 'test']).default('production'),
   VITE_MAINTENANCE_MODE: z
     .enum(['true', 'false'])
-    .transform((value) => value === 'true'),
+    .transform((value) => value === 'true')
+    .default('false'),
 })
 
 export const env = envSchema.parse(import.meta.env)
